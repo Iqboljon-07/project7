3f761ec732eb0b9fe9de3aed31390330eb62da56
Iqboljon Orifjonov <orifjonoviqboljon@gmail.com>
2025-02-01 00:33:59 +0500

    add member togirlandi

--- src/components/members/Members.jsx
+++ src/components/members/Members.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
-
 import { useStateValue } from "../../context";
-import { Style } from "@mui/icons-material";
 import { useNavigate, useParams } from "react-router-dom";
 import CloseIcon from "@mui/icons-material/Close";
 import axios from "axios";
 import { toast } from "react-toastify";
-const Members = () => {
-  const { membermodal, setMemberModal } = useStateValue();
-  const { members, setMembers } = useStateValue();
-  const { groupId } = useParams();
-  //console.log(groupId);
-  const navigate = useNavigate();
 
+const Members = () => {
   const {
+    membermodal,
+    setMemberModal,
+    members,
+    setMembers,
     show,
     setShow,
     detail,
@@ -23,12 +20,14 @@ const Members = () => {
     setMemberSearches,
   } = useStateValue();
 
+  const { groupId } = useParams();
+
+  const [isPending, setIsPending] = useState(false);
+
   const onChange = async (member) => {
-    //console.log(member);
     try {
       let response = await axios.get(
         `https://nt-shopping-list.onrender.com/api/users/search?q=${member}`,
-        {},
         {
           headers: {
             "x-auth-token": localStorage.getItem("token"),
@@ -46,49 +45,46 @@ const Members = () => {
 
   const Addmember = async (val) => {
     try {
+      setIsPending(true);
       const response = await axios.post(
         `https://nt-shopping-list.onrender.com/api/groups/${groupId}/members`,
         { memberId: val._id },
         {
           headers: {
             "x-auth-token": `${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
           },
         }
       );
-      //console.log(response);
+
       if (response.status === 200) {
-        setisPending("");
         toast.success(response.data.message);
         setMemberModal(false);
-        setMembers((prev) => [...prev, val]);
+        setMembers([...members, val]);
       }
     } catch (err) {
       console.log(err);
       toast.error("Error");
+    } finally {
+      setIsPending(false);
     }
   };
 
   return (
     <>
-      <div className={`members `}>
-        <div className="members_item   ">
+      <div className={`members ${membermodal ? "show" : ""}`}>
+        <div className="members_item">
           <div
             style={{
               display: "flex",
               height: "90px",
-
               justifyContent: "space-between",
-
               alignItems: "center",
               borderBottom: "1px solid grey",
             }}
           >
             <h3>Add Member</h3>
-            <CloseIcon
-              onClick={() => {
-                setMemberModal(false);
-              }}
-            />
+            <CloseIcon onClick={() => setMemberModal(false)} />
           </div>
           <input
             onChange={(e) => onChange(e.target.value)}
@@ -101,18 +97,15 @@ const Members = () => {
             type="text"
             placeholder="Search user"
           />
-
           <ul>
             {memberSearches.map((val) => (
               <li onClick={() => Addmember(val)} key={val._id}>
-                {/* id o'rniga val o'zini bervorib tepadada id tutvolamiz */}
                 {val.name}
               </li>
             ))}
           </ul>
         </div>
       </div>
-      ;
     </>
   );
 };
